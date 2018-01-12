a54c7bd21d42c27ad87789ba36a9a71b0c3a299c
Pavel Vashchuk <pavelvashchuk@gmail.com>
2018-01-12 14:48:36 -0500

    added page1

--- index.php
+++ index.php
@@ -12,9 +12,24 @@ require_once ('vendor/autoload.php');
 //create an instance of the Base class
 $f3 = Base::instance();
 
+//set debug level
+$f3->set('DEBUG', 3);
+
 //define a default route
-$f3->route('GET /', function() {
+$f3->route("GET /", function() {
     echo '<h1>Routing Demo</h1>';
+}
+);
+
+//define a page1 route
+$f3->route("GET /page1", function() {
+    echo '<h1>This is page 1</h1>';
+}
+);
+
+//define a page1, subpage a route
+$f3->route("GET /page1/subpage-a", function() {
+    echo '<h1>This is page 1, subpage a</h1>';
 }
 );
 
