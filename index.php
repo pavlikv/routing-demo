c12f385996e32a2775e8a8013127ea26909fc2a0
Pavel Vashchuk <pavelvashchuk@gmail.com>
2018-01-16 13:55:15 -0500

    Added hello.html

--- index.php
+++ index.php
@@ -23,8 +23,13 @@ $f3->route("GET /", function() {
 
 //define a route using parameters
 $f3->route("GET /hello/@name", function($f3, $params) {
-    $name = $params['name'];
-    echo "<h1>Hello, $name</h1>";
+    //$name = $params['name'];
+    //echo "<h1>Hello, $name</h1>";
+
+    //adding variables to the 'hive' (saving it to usse from the template"
+    $f3->set('name', $params['name']);
+    $template = new Template();
+    echo $template->render('views/hello.html');
 }
 );
 
